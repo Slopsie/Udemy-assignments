3d6799ab30427388f964715a49b4531624de25bb
Slopsie <slopsie@gmail.com>
2017-10-24 11:46:47 +0200

    Add files via upload
    Using else/if statements to determine time of day.

---
+++ conditionalstatements1/conditionalstatements1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace conditionalstatements1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning");
+            }
+            
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's in the afternoon");
+            }
+
+            else
+            {
+                Console.WriteLine("It's evening");
+            }
+        }
+    }
+}
