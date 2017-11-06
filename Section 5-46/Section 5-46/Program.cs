e77140c78b1df6aec44053150bcb2f437810585f
Slopsie <slopsie@gmail.com>
2017-11-06 16:41:34 +0100

    Add files via upload
    Using while loops to echo names and break the loop

---
+++ Section 5-46/Section 5-46/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+
+namespace Section_5_46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Type your name here: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
